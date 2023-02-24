751c962f3092f02dea792f6ce7ff58177ff98952
Ari Wijaya <ariwijaya.its@gmail.com>
2023-02-24 16:37:19 +0700

    react can rn

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
 module.exports = {
+  mode: "development",
   entry: './src/index',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -13,12 +13,24 @@ module.exports = {
   },
   module: {
     rules: [{
-      test: /\.(ts|js)x?$/,
+      test: /\.tsx?$/,
       exclude: '/node_modules/',
       loader: 'babel-loader'
     }]
   },
   plugins: [
-    new HtmlWebpackPlugin()
-  ]
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html")
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public')
+    },
+    compress: true,
+    port: 9000,
+  },
+  stats: {
+    errorDetails: true
+  }
 }
